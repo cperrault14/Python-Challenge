e5790577f7af5e86c5aedf41c8fc55f809ca8017
Christina <cperrault14@users.noreply.github.com>
2021-02-27 21:18:00 -0500

    Added printing values and output code to support export to text_file

--- PyBank/main.py
+++ PyBank/main.py
@@ -28,7 +28,7 @@ with open(r"C:\Users\cperr\Desktop\Python-Challenge\PyBank\Resources\budget_data
         profit_loss_change_list = profit_loss_change_list + [profit_loss_change]
         month_change = month_change + [row[0]]
         # print(month_change)
-        #Creating conditionals to set what the profit_loss_change index will be, 0 or 1
+        #Creating conditionals to set what the profit_loss_change index based on month row
         if profit_loss_change > greatest_increase[1]:
             greatest_increase[0] = row[0]
             greatest_increase[1] = profit_loss_change
@@ -37,6 +37,25 @@ with open(r"C:\Users\cperr\Desktop\Python-Challenge\PyBank\Resources\budget_data
             greatest_decrease [1] = profit_loss_change
 #Calculating the average change by diving the sum of the profit_loss_change list with the count of rows available in that list
 average_change = sum(profit_loss_change_list)/len(profit_loss_change_list)
-print("Total Month:", month)
-print(f"Net Total Amount: ${total_net_amt}\n")
-print(f"Averange Change: ${average_change}\n")
+#Printing all results as per instructions
+print("Financial Analysis")
+print("--------------------------------------------------")
+print("Total Months:", month)
+print(f"Total: ${total_net_amt}\n")
+print(f"Average Change: ${average_change}\n")
+print(f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})")
+print(f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})")
+
+#Summary of results as Output
+output = (
+   f"\nFinancial Analysis\n"
+   f"----------------------------\n"
+   f"Total Months: {month}\n"
+   f"Total: ${total_net_amt}\n"
+   f"Average  Change: ${average_change}\n"
+   f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n"
+   f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
+
+#Exporting summary table to textfile
+with open("C:\\Users\\cperr\\Desktop\\Python-Challenge1\\PyBank\\Analysis\\bank.txt", 'w') as text_file:
+    text_file.write(output)
