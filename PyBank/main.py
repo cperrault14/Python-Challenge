492a0df4759c40f8956f492863374f0eb41187f1
Christina <cperrault14@users.noreply.github.com>
2021-02-27 21:04:47 -0500

    Calculated the Average change. Need to summarize data on table and export results to text file

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,15 +11,19 @@ with open(r"C:\Users\cperr\Desktop\Python-Challenge\PyBank\Resources\budget_data
     csv_reader = reader(file, delimiter=',')
     next(csv_reader, None)
 
-
+    first_row = next(csv_reader)
+    month = month + 1
+    total_net_amt = total_net_amt + int(first_row[1])
+    # print("Total Month:", month)
+    # print(f"Net Total Amount: ${total_net_amt}\n")
+    previous_change = int(first_row[1])
 #Looping through the csvfile to return desired results
     for row in csv_reader:
         month = month + 1
         total_net_amt = total_net_amt + int(row[1])
-        # print("Total Month:", month)
-        # print(f"Net Total Amount: ${total_net_amt}\n")
-        previous_change = int(row[1])
+
         profit_loss_change = int(row[1]) - previous_change
+        previous_change = int(row[1])
         # print(profit_loss_change)
         profit_loss_change_list = profit_loss_change_list + [profit_loss_change]
         month_change = month_change + [row[0]]
@@ -31,28 +35,8 @@ with open(r"C:\Users\cperr\Desktop\Python-Challenge\PyBank\Resources\budget_data
         if profit_loss_change < greatest_decrease[1]:
             greatest_decrease[0] = row[0]
             greatest_decrease [1] = profit_loss_change
-# #Calculating the change from one month to the next by 
-# #Creating a new column and record the value by shifting the current month 1 row down.
-
-
-# #changes of profit_loss calculations
-# profit_loss_change = int(row[1]) - profit_loss_previous
-# profit_loss_previous = int(row[1])
-# month_change = month_change + [row[0]]
-# # print(month_change)
-
-# #Greatest Increase value
-# if (profit_loss_change > greatest_increase[1]):
-#     greatest_increase[1] = profit_loss_change
-#     greatest_increase[0] = row[0]
-#     if (profit_loss_change < greatest_decrease[1]):
-#         greatest_decrease[0] = row[0]
-#         greatest_decrease[1] = profit_loss_change
-# if len(profit_loss_change_list) !=0:
-#     average_change = sum(profit_loss_change_list) / len(profit_loss_change_list)
-# else:
-#      average_change = 0
-
-# print("Average Change:", average_change)
-
-# #average_change = sum(differences) / len(differences)
+#Calculating the average change by diving the sum of the profit_loss_change list with the count of rows available in that list
+average_change = sum(profit_loss_change_list)/len(profit_loss_change_list)
+print("Total Month:", month)
+print(f"Net Total Amount: ${total_net_amt}\n")
+print(f"Averange Change: ${average_change}\n")
